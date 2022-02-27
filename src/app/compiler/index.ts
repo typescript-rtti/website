1be24ab83f28b240d45f72a03b1355087fe7cf68
William Lahti <wilahti@gmail.com>
2022-02-26 18:22:13 -0800

    only one version of tsrtti

--- src/app/compiler/index.ts
+++ src/app/compiler/index.ts
@@ -1,6 +1,6 @@
 import * as ts from '../../../supported-typescript/node_modules/typescript';
 import transformer from '../../../supported-typescript/node_modules/typescript-rtti/dist/transformer';
-import * as lib from 'typescript-rtti';
+import * as lib from '../../../supported-typescript/node_modules/typescript-rtti';
 
 export const Compiler = {
     ts, transformer, lib
