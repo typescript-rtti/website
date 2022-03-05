98b10f696ed6492581338155bfedcfb8d1435bf4
William Lahti <wilahti@gmail.com>
2022-03-05 05:54:03 -0800

    typescript-rtti@0.4.12

--- src/app/playground.service.ts
+++ src/app/playground.service.ts
@@ -6,7 +6,7 @@ import type { Compiler } from './compiler';
 
 let stdLibraries : Record<string,string> = undefined;
 
-export const RTTI_VERSION = '0.4.10';
+export const RTTI_VERSION = '0.4.12';
 
 @Injectable()
 export class PlaygroundService {
